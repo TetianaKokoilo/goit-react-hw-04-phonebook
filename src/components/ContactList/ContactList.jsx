53c7cdba776f969854792683f9e2cb06313e10dc
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-03-12 15:57:14 +0200

    add useState for App

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,10 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { StyledText, StyledList, StyledItems, StyledContactButton } from './ContactList.styled'
 
 export const ContactList = ({ contacts, onDelete }) => {
   return (
     <StyledList>
-      {contacts.map(({ id, name, number }) => (
+      {contacts().map(({ id, name, number }) => (
         <StyledItems key={id}>
           <StyledText>{name}:</StyledText>
           <StyledText>{number}</StyledText>
@@ -17,12 +17,12 @@ export const ContactList = ({ contacts, onDelete }) => {
   );
 };
 
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-};
+// ContactList.propTypes = {
+//   contacts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string,
+//       name: PropTypes.string,
+//       number: PropTypes.string,
+//     })
+//   ),
+// };
